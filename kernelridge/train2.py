23059c70d0b30c22e9376632f66bf6108777d068
Your Name <you@example.com>
2025-09-30 01:20:54 -0400

    Add KernelRidge model code

---
+++ kernelridge/train2.py
@@ -0,0 +1,9 @@
+from sklearn.kernel_ridge import KernelRidge
+from misc import load_data, preprocess, train_model, evaluate_model
+
+df = load_data()
+X_train, X_test, y_train, y_test = preprocess(df)
+model = KernelRidge()
+model = train_model(model, X_train, y_train)
+mse = evaluate_model(model, X_test, y_test)
+print(f"Kernel Ridge MSE: {mse:.2f}")
